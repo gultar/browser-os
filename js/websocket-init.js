af2813b14f2e2547ace1f8dea1316470e62bdf8d
gultar <sacha-olivier.dulac@hotmail.com>
2022-12-17 18:39:37 -0500

    fix bug with ping server not allowing commands

--- js/websocket-init.js
+++ js/websocket-init.js
@@ -1,25 +1,3 @@
-
-const execRemoteCommand = (cmd, args=[]) =>{
-  return new Promise((resolve)=>{
-    try{
-      
-      socket.once("shell-result", (shellResult)=>{
-        if(shellResult.error){
-          resolve({ error:shellResult.error })
-        }
-        else{
-          const { result } = shellResult
-          console.log('Exec cmd', cmd, result)
-          resolve(result)
-        }
-      })
-      socket.emit("shell-command", cmd, args)
-    }catch(e){
-      resolve({ error:e })
-    }
-  })
-}
-
 const execServerCommand = async (cmd, args=[]) =>{
   const {error, result} = await Promise.resolve($.post("http://localhost:8000/command", {
       cmd:cmd,
